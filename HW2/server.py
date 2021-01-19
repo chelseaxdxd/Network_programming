bc335f23a8fa7d28b8b247d6247bcb3b47179d49
Chelsea <870313amy@gmail.com>
2021-01-19 23:30:39 +0800

    change server.py to a no-error demo version

--- HW2/server.py
+++ HW2/server.py
@@ -1,16 +1,36 @@
 from socket import *
 import sys
 import select
-import _thread
+import threading,queue
 import sqlite3
 from sqlite3 import Error
 from random import seed
 from random import randint
+import time
+lock = threading.Lock()
+class Post:
+	def __init__(self, bname, pid, title, username, date, content):
+		self.bname = bname
+		self.pid = pid
+		self.title = title
+		self.username = username
+		self.date = date
+		self.content = content
+		self.comments = []
+	def add_new_comment(self,name,comment):
+		self.comments.append((name,comment))
+		#for comment in self.comments:
+			#print(comment[0],comment[1])
 
-def tcpThread(childSocket, addr):
+
+
+
+
+def tcpThread(childSocket, addr,q,posts):
 	# Initialize
 	login = 0 ## check login
 	name = None
+	
 	while True:
 		recBuf = childSocket.recv(1024).decode('utf-8')#傳送的訊息是byte，要換成string
 		recBuf_split = recBuf.split()
@@ -111,18 +131,152 @@ def tcpThread(childSocket, addr):
 					childSocket.send(sendBuf.encode('utf-8'))
 					conn.commit()
 					conn.close()
+		##list-board
+		if recBuf.startswith("list-board"):
+			##Index Name Moderator
+			conn = sqlite3.connect('store.db')
+			c = conn.cursor()
+			cursor = c.execute("SELECT * FROM BOARDS").fetchall()
+			sendBuf = "{:^7} {:<20} {:<20} \r\n".format("Index", "Name", "Moderator")
+			for i in range(len(cursor)):
+				sendBuf = sendBuf+ "{:^7} {:<20} {:<20}\r\n".format(i,cursor[i][0], cursor[i][1])
+			#i = 1
+			#for row in cursor:
+			#	sendBuf = sendBuf+ "{:^7} {:<20} {:<20}\r\n".format(i,row[0], row[1])
+			#	i = i+1
+			childSocket.send(sendBuf.encode('utf-8'))
 
-		##create-post 
+		##create-post (pid, title, username, date, content)
 		if recBuf.startswith("create-post"):
 			if login == 0:
 				sendBuf = 'Please login first.'
 				childSocket.send(sendBuf.encode('utf-8'))
 			elif len(recBuf_split) > 5 and TITLE in recBuf and CONTENT in recBuf:
 				bname = recBuf_split[1]
-				post = recBuf.replace("create-post ", "").replace(bname,"").replace(" --title ","")
-				post_split = post.split(" --content ")
-				title = post_split[0]
-				content = post_split[1]
+
+				#連接DB
+				conn = sqlite3.connect('store.db')
+				c = conn.cursor()
+				cursor = c.execute("SELECT * FROM BOARDS WHERE Bname = ?",(bname,))
+				found = cursor.fetchone()
+				#若不存在此 user name
+				if found == None:
+					sendBuf = 'Board does not exist.'
+					childSocket.send(sendBuf.encode('utf-8'))
+				else:
+					post = recBuf.replace("create-post ", "").replace(bname,"").replace(" --title ","")
+					post_split = post.split(" --content ")
+					title = post_split[0]
+					content = post_split[1]
+					date = time.strftime("%m/%d", time.localtime()) ## is a string
+					##share memory access
+					lock.acquire()
+		
+					pid = q.get()
+					posts.append(Post(bname, pid, title, name, date,content))
+					pid=pid+1
+					q.put(pid)
+					lock.release()
+
+					sendBuf = 'Create post successfully.'
+					childSocket.send(sendBuf.encode('utf-8'))
+
+		##list-post <board-name>
+		if recBuf.startswith("list-post"):
+			if next((post for post in posts if post.bname == recBuf_split[1]), None) == None: ##若沒有找到就回傳none
+				sendBuf = 'Board does not exist.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			else:
+				sendBuf = "{:^7} {:^20} {:^20} {:^9}\r\n\r\n".format("S/N", "Title", "Author", "Date")
+				for post in posts:
+					if post.bname == recBuf_split[1]:
+						sendBuf = sendBuf + "{:^7} {:^20} {:^20} {:^9}\r\n\r\n".format(post.pid,post.title, post.username, post.date)
+				childSocket.send(sendBuf.encode('utf-8'))
+
+		##read <post-S/N>
+		if recBuf.startswith("read"):
+			if next((post for post in posts if post.pid == int(recBuf_split[1])), None) == None: ##若沒有找到就回傳none
+				sendBuf = 'Post does not exist.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			else:
+				for post in posts:
+					if post.pid == int(recBuf_split[1]):
+						post.content = post.content.replace("<br>", "\n")
+						sendBuf = "Author: "+ post.username + "\nTitle: "+post.title+"\nDate: "+post.date +"\n--\n"+post.content + "\n--\n"
+						for comment in post.comments:
+							sendBuf = sendBuf + comment[0] + ': '+comment[1]+'\n'
+						childSocket.send(sendBuf.encode('utf-8'))
+
+		##comment <post-S/N> <comment> 
+		if recBuf.startswith("comment"):
+			if login == 0:
+				sendBuf = 'Please login first.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			elif next((post for post in posts if post.pid == int(recBuf_split[1])), None) == None: ##若沒有找到就回傳none
+				sendBuf = 'Post does not exist.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			else:
+				for post in posts:
+					if post.pid == int(recBuf_split[1]):
+						comment = recBuf.replace("comment ", "").replace(str(post.pid)+' ',"")
+						print("comment= ",comment)
+						post.add_new_comment(name,comment)
+						sendBuf = 'Comment successfully.'
+						childSocket.send(sendBuf.encode('utf-8'))
+
+		##update-post <post-S/N> --title/content<new>
+		#update-post 1 --title NP HW_2
+		#(pid, title, username, date, content)
+		if recBuf.startswith("update-post"):
+			if login == 0:
+				sendBuf = 'Please login first.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			elif next((post for post in posts if post.pid == int(recBuf_split[1])), None) == None: ##若沒有找到就回傳none
+				sendBuf = 'Post does not exist.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			elif next((post for post in posts if post.pid == int(recBuf_split[1]) and post.username == name), None) == None:
+				sendBuf = 'Not the post owner.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			else:
+				for i in range(len(posts)):
+					if posts[i].pid == int(recBuf_split[1]):
+						if recBuf_split[2] == TITLE:
+							lock.acquire()
+							posts[i].title = recBuf.split("--title ")[1]
+							lock.release()
+							sendBuf = 'Update successfully'
+							childSocket.send(sendBuf.encode('utf-8'))							
+						elif recBuf_split[2] == CONTENT:
+							lock.acquire()
+							posts[i].content = recBuf.split("--content ")[1]
+							lock.release()
+							sendBuf = 'Update successfully'
+							childSocket.send(sendBuf.encode('utf-8'))
+						else:
+							sendBuf = 'update-post <post-S/N> --title/content<new>'
+							childSocket.send(sendBuf.encode('utf-8'))
+
+
+		##delete-post <post-S/N>
+		if recBuf.startswith("delete-post"):
+			if login == 0:
+				sendBuf = 'Please login first.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			elif next((post for post in posts if post.pid == int(recBuf_split[1])), None) == None: ##若沒有找到就回傳none
+				sendBuf = 'Post does not exist.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			elif next((post for post in posts if post.pid == int(recBuf_split[1]) and post.username == name), None) == None:
+				sendBuf = 'Not the post owner.'
+				childSocket.send(sendBuf.encode('utf-8'))
+			else:
+				for i in range(len(posts)):
+					if posts[i].pid == int(recBuf_split[1]):
+						del posts[i]
+						sendBuf = 'Delete successfully.'
+						childSocket.send(sendBuf.encode('utf-8'))
+						break
+					
+				
 
 		##exit
 		if recBuf.startswith("exit"):
@@ -132,8 +286,9 @@ def tcpThread(childSocket, addr):
 			break
 	
 #initial
-TITLE = " --title "
-CONTENT = " --content "
+TITLE = "--title"
+CONTENT = "--content"
+
 
 #initial db
 
@@ -167,14 +322,17 @@ udpSocket = socket(AF_INET, SOCK_DGRAM)
 udpSocket.bind(('127.0.0.1', port))
 
 input = [tcpSocket,udpSocket]
-
+q = queue.Queue()
+posts = []
+q.put(1)
 while True:
 	inputready,_,_ = select.select(input,[],[])
 	for sck in inputready:
 		if sck is tcpSocket:
 			childSocket,addr = tcpSocket.accept()
 			print ('New connection :', addr)
-			_thread.start_new_thread(tcpThread, (childSocket, addr))
+			t = threading.Thread(target = tcpThread,args= (childSocket, addr,q,posts))
+			t.start()
 		elif sck is udpSocket:
 			recBuf,addr = udpSocket.recvfrom(1024)
 			recBuf=recBuf.decode('utf-8')
@@ -226,4 +384,3 @@ while True:
 						conn.close()
 						sendBuf = found[0]
 						udpSocket.sendto(sendBuf.encode('utf-8'), addr)
-		
